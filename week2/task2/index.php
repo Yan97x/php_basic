5031bf6ed631af4c729252b8a0a61b89aec6a119
www-data <www-data@s5191915.elf.ict.griffith.edu.au>
2021-08-03 18:46:11 +1000

    Task 2

--- week2/task2/index.php
+++ week2/task2/index.php
@@ -1,3 +1,27 @@
 <?php
-    echo "yan";
+    include 'classes/post.php';
+    $post = new Post("Yan", "Hi!");
+    var_dump($post->getUser());
+    exit;
 ?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Social Media</title>
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
+        <div id = "content">
+            <h1>Social Media</h1>
+            <?php foreach($posts as $post) { ?>
+                <div id = "post">
+                <img src="<?= $post['image'] ?>" width="60" height="60" alt="user image">
+                <?= $post['name'] ?>
+                <?= $post['message'] ?>
+                <?= $post['date'] ?>
+                </div>
+            <?php } ?>
+        </div>
+    </body>
+</html>
