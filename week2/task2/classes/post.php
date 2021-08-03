d9c24d810a58c8b647f50312fcc6347d9a9c4fca
liyan1997s <liyan1997s@gmail.com>
2021-08-03 20:38:37 +1000

    update

--- week2/task2/classes/post.php
+++ week2/task2/classes/post.php
@@ -11,7 +11,7 @@ class Post{
         $this->user = $user;
         $this->message = $message;
         $this->date = $date;
-        $this->date = '../images/default.jpg';
+        $this->date = '../images/defult.jpg';
         $this->comments = [];
     }
 
